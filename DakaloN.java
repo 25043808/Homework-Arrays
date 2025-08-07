5c524078b0d5b8fa4ca80e1d6ba527a50523cda6
25043808 <25043808@mvula.univen.ac.za>
2025-08-07 22:36:59 +0200

    Add files via upload

--- DakaloN.java
+++ DakaloN.java
@@ -7,7 +7,7 @@ public class DakaloN {
     public static void Children(String[][]seats){
         for( int row=0;row<seats.length;row++){
             for( int colum=0;colum<seats[row].length;colum++){
-                System.out.println("row"+(row+1)+" , Seat " + (colum+1) + " : " + seats[row][col]);
+                System.out.println("row"+(row+1)+" , Seat " + (colum+1) + " : " + seats[row][colum]);
             }
         }
     }
